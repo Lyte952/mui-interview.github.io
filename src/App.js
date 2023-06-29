b3075033e997df99cbce10205ca37fdc1c4fe549
Lyte <107444079+Lyte952@users.noreply.github.com>
2023-06-29 20:46:36 +0000

    Added color upgrades

--- src/App.js
+++ src/App.js
@@ -7,19 +7,18 @@ const App = () => {
     flexDirection: 'column',
     justifyContent: 'center',
     alignItems: 'center',
-    height: '100vh',
-    borderRadius: '12px'
+    borderRadius: '16px'
   };
 
   const textFieldStyle = {
     marginBottom: '16px',
-    borderRadius: '12px'
+    borderRadius: '16px',
+
   };
 
   const topButtons = {
     margin: '5px',
-    color: 'white',
-    textTransform: 'none'
+    textTransform: 'none',
   };
 
   const buttonStyle = {
@@ -30,22 +29,38 @@ const App = () => {
 
   return (
     <div>
-      <AppBar position="static">
-        <Toolbar>
+      <AppBar position="static" >
+        <Toolbar sx={{backgroundColor: '#19d2b3'}}>
           <Typography variant="h6" component="div" style={{ flexGrow: 1 }}>
-            My Navbar
+            Product Name
           </Typography>
-          <Button variant="contained" style={topButtons} >Login</Button>
-          <Button variant="contained" style={topButtons} >Sign Up</Button>
+          <Button variant="contained" style={topButtons} sx={{backgroundColor: 'white', 
+                                                              color: '#19d2b3', 
+                                                                ':hover': {bgcolor: '#19d2b3', 
+                                                                           color: 'white'}}}>Login</Button>
+          <Button variant="contained" style={topButtons} sx={{backgroundColor: 'white',
+                                                              color: '#19d2b3', 
+                                                                ':hover': {bgcolor: '#19d2b3', 
+                                                                           color: 'white'}}}>Sign Up</Button>
         </Toolbar>
       </AppBar>
 
       <div style={containerStyle}>
+        <Typography style={{margin: '20px', fontSize:'40px', color: '#19d2b3', marginBottom: '80px'}}>Login</Typography>
         <Card>
           <CardContent style={containerStyle}>
-            <TextField type="email" placeholder='email' style={textFieldStyle} />
-            <TextField type="password" placeholder='password' style={textFieldStyle} />
-            <Button variant="contained" color="primary" style={buttonStyle} >Submit</Button>
+            <TextField type="text" variant='outlined' placeholder='Username' style={textFieldStyle} sx={{'& input': {borderColor: '#19d2b3'}}}/>
+            <TextField type="password" variant='outlined' placeholder='Password' style={textFieldStyle} sx={{'& input': {borderColor: '#19d2b3'}}} />
+            <Button variant="contained" style={buttonStyle} sx={{backgroundColor: '#19d2b3', ':hover': {bgcolor: 'white', 
+                                                                                                        color: '#19d2b3'}}}>Submit</Button>
+          </CardContent>
+        </Card>
+      </div>
+
+      <div>
+        <Card sx={{bgcolor: '#00a388', marginTop: '200px'}}>
+          <CardContent>
+            <Typography sx={{justifyContent: 'center', color: 'white'}}>Login or Sign Up to enjoy the features of our product!!!</Typography>
           </CardContent>
         </Card>
       </div>
