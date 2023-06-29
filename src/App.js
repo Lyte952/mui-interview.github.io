e83f6032d0bea5e50f3c00820bea4888e8f2b60b
Lyte <107444079+Lyte952@users.noreply.github.com>
2023-06-29 22:36:33 +0000

    Implemented React

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import { Card, CardContent, TextField, AppBar, Toolbar, Typography, Button } from '@mui/material';
 
-const App = () => {
-  const containerStyle = {
+class Login extends React.Component {
+  containerStyle = {
     display: 'flex',
     flexDirection: 'column',
     justifyContent: 'center',
@@ -10,62 +10,104 @@ const App = () => {
     borderRadius: '16px'
   };
 
-  const textFieldStyle = {
+  textFieldStyle = {
     marginBottom: '16px',
     borderRadius: '16px',
-
   };
 
-  const topButtons = {
-    margin: '5px',
-    textTransform: 'none',
-  };
-
-  const buttonStyle = {
+  buttonStyle = {
     height: '26px',
     textTransform: 'none'
   };
 
-
-  return (
-    <div>
-      <AppBar position="static" >
-        <Toolbar sx={{backgroundColor: '#19d2b3'}}>
-          <Typography variant="h6" component="div" style={{ flexGrow: 1 }}>
-            Product Name
-          </Typography>
-          <Button variant="contained" style={topButtons} sx={{backgroundColor: 'white', 
-                                                              color: '#19d2b3', 
-                                                                ':hover': {bgcolor: '#19d2b3', 
-                                                                           color: 'white'}}}>Login</Button>
-          <Button variant="contained" style={topButtons} sx={{backgroundColor: 'white',
-                                                              color: '#19d2b3', 
-                                                                ':hover': {bgcolor: '#19d2b3', 
-                                                                           color: 'white'}}}>Sign Up</Button>
-        </Toolbar>
-      </AppBar>
-
-      <div style={containerStyle}>
-        <Typography style={{margin: '20px', fontSize:'40px', color: '#19d2b3', marginBottom: '80px'}}>Login</Typography>
-        <Card>
-          <CardContent style={containerStyle}>
-            <TextField type="text" variant='outlined' placeholder='Username' style={textFieldStyle} sx={{'& input': {borderColor: '#19d2b3'}}}/>
-            <TextField type="password" variant='outlined' placeholder='Password' style={textFieldStyle} sx={{'& input': {borderColor: '#19d2b3'}}} />
-            <Button variant="contained" style={buttonStyle} sx={{backgroundColor: '#19d2b3', ':hover': {bgcolor: 'white', 
-                                                                                                        color: '#19d2b3'}}}>Submit</Button>
-          </CardContent>
-        </Card>
-      </div>
-
+  render() {
+    return (
       <div>
-        <Card sx={{bgcolor: '#00a388', marginTop: '200px'}}>
-          <CardContent>
-            <Typography sx={{justifyContent: 'center', color: 'white'}}>Login or Sign Up to enjoy the features of our product!!!</Typography>
-          </CardContent>
-        </Card>
+        <div style={this.containerStyle}>
+          <Typography style={{ margin: '20px', fontSize: '40px', color: '#19d2b3', marginBottom: '80px' }}>Login</Typography>
+          <Card>
+            <CardContent style={this.containerStyle}>
+              <TextField type="text" variant="outlined" placeholder="Username" style={this.textFieldStyle} InputProps={{ style: { borderColor: '#19d2b3' } }} />
+              <TextField type="password" variant="outlined" placeholder="Password" style={this.textFieldStyle} InputProps={{ style: { borderColor: '#19d2b3' } }} />
+              <Button variant="contained" style={this.buttonStyle} sx={{ backgroundColor: '#19d2b3', ':hover': { backgroundColor: 'white', color: '#19d2b3' } }}>Submit</Button>
+            </CardContent>
+          </Card>
+        </div>
       </div>
-    </div>
-  );
-};
+    );
+  }
+}
+
+class Signup extends React.Component {
+  containerStyle = {
+    display: 'flex',
+    flexDirection: 'column',
+    justifyContent: 'center',
+    alignItems: 'center',
+    borderRadius: '16px'
+  };
+
+  textFieldStyle = {
+    marginBottom: '16px',
+    borderRadius: '16px',
+  };
+
+  buttonStyle = {
+    height: '26px',
+    textTransform: 'none'
+  };
+
+  render() {
+    return (
+      <div>
+        <div style={this.containerStyle}>
+          <Typography style={{ margin: '20px', fontSize: '40px', color: '#19d2b3', marginBottom: '80px' }}>Signup</Typography>
+          <Card>
+            <CardContent style={this.containerStyle}>
+              <TextField type="text" variant="outlined" placeholder="Name" style={this.textFieldStyle} InputProps={{ style: { borderColor: '#19d2b3' } }} />
+              <TextField type="password" variant="outlined" placeholder="Preferred username" style={this.textFieldStyle} InputProps={{ style: { borderColor: '#19d2b3' } }} />
+              <TextField type="password" variant="outlined" placeholder="Type preferred password" style={this.textFieldStyle} InputProps={{ style: { borderColor: '#19d2b3' } }} />
+              <TextField type="password" variant="outlined" placeholder="Confirm password" style={this.textFieldStyle} InputProps={{ style: { borderColor: '#19d2b3' } }} />
+              <Button variant="contained" style={this.buttonStyle} sx={{ backgroundColor: '#19d2b3', ':hover': { backgroundColor: 'white', color: '#19d2b3' } }}>Submit</Button>
+            </CardContent>
+          </Card>
+        </div>
+      </div>
+    );
+  }
+}
+
+class App extends React.Component {
+  topButtons = {
+    margin: '5px',
+    textTransform: 'none',
+  };
+
+  render() {
+    return (
+      <div>
+        <AppBar position="static" sx={{ backgroundColor: '#19d2b3' }}>
+          <Toolbar>
+            <Typography variant="h6" component="div" style={{ flexGrow: 1 }}>
+              Product Name
+            </Typography>
+            <Button value="login" variant="contained" style={this.topButtons} sx={{ backgroundColor: 'white', color: '#19d2b3', ':hover': { backgroundColor: '#19d2b3', color: 'white' } }}>Login</Button>
+            <Button value="signup" variant="contained" style={this.topButtons} sx={{ backgroundColor: 'white', color: '#19d2b3', ':hover': { backgroundColor: '#19d2b3', color: 'white' } }}>Sign Up</Button>
+          </Toolbar>
+        </AppBar>
+
+        <Login />
+
+        <div>
+          <Card sx={{ bgcolor: '#00a388', marginTop: '200px' }}>
+            <CardContent>
+              <Typography sx={{ textAlign: 'center', color: 'white' }}>Login or Sign Up to enjoy the features of our product!!!</Typography>
+            </CardContent>
+          </Card>
+        </div>
+      </div>
+    );
+  }
+}
 
 export default App;
