3539363645c3b111459126a011c40ea846a4f276
Lyte <107444079+Lyte952@users.noreply.github.com>
2023-06-29 19:13:19 +0000

    Added upgrades

--- src/App.js
+++ src/App.js
@@ -1,35 +1,56 @@
 import React from 'react';
-import * as MUI from '@mui/material';
-import { AppBar, Toolbar, Typography, Button } from '@mui/material';
+import { Card, CardContent, TextField, AppBar, Toolbar, Typography, Button } from '@mui/material';
 
-class Top extends React.Component {
-  render(){
-    return(
-      <div>
-        <AppBar position="static">
-          <Toolbar>
-            <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
-              My Navbar
-            </Typography>
-            <Button color="inherit">Home</Button>
-            <Button color="inherit">About</Button>
-            <Button color="inherit">Services</Button>
-            <Button color="inherit">Contact</Button>
-          </Toolbar>
-        </AppBar>
-        <MUI.Button variant="contained"color="primary" px="10px" py="10px">Click Me</MUI.Button>
-      </div>
-    )
-  }
-}
+const App = () => {
+  const containerStyle = {
+    display: 'flex',
+    flexDirection: 'column',
+    justifyContent: 'center',
+    alignItems: 'center',
+    height: '100vh',
+    borderRadius: '12px'
+  };
 
-function App() {
+  const textFieldStyle = {
+    marginBottom: '16px',
+    borderRadius: '12px'
+  };
+
+  const topButtons = {
+    margin: '5px',
+    color: 'white',
+    textTransform: 'none'
+  };
+
+  const buttonStyle = {
+    height: '26px',
+    textTransform: 'none'
+  };
+
+
   return (
     <div>
-      <Top />
-      <h1>Hello, World!</h1>
+      <AppBar position="static">
+        <Toolbar>
+          <Typography variant="h6" component="div" style={{ flexGrow: 1 }}>
+            My Navbar
+          </Typography>
+          <Button variant="contained" style={topButtons} >Login</Button>
+          <Button variant="contained" style={topButtons} >Sign Up</Button>
+        </Toolbar>
+      </AppBar>
+
+      <div style={containerStyle}>
+        <Card>
+          <CardContent style={containerStyle}>
+            <TextField type="email" placeholder='email' style={textFieldStyle} />
+            <TextField type="password" placeholder='password' style={textFieldStyle} />
+            <Button variant="contained" color="primary" style={buttonStyle} >Submit</Button>
+          </CardContent>
+        </Card>
+      </div>
     </div>
   );
-}
+};
 
 export default App;
