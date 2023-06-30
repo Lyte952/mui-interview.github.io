fa19cf7ad780e4ac55abe7e4ba6b3e5179e3e0e2
Lyte <107444079+Lyte952@users.noreply.github.com>
2023-06-30 20:20:51 +0000

    More UI

--- src/App.js
+++ src/App.js
@@ -56,12 +56,12 @@ class Login extends React.Component {
     return (
       <div>
         <div style={this.containerStyle}>
-          <Typography style={{ margin: '20px', fontSize: '40px', color: '#19d2b3', marginBottom: '80px', fontWeight: 'bold' }}>Login</Typography>
+          <Typography style={{ margin: '20px', fontSize: '40px', color: '#19d2b3', marginBottom: '45px', fontWeight: 'bold' }}>Login</Typography>
           <Card>
             <CardContent style={this.containerStyle}>
               <TextField id="username" onChange={this.handleChange} type="text" variant="outlined" placeholder="Username" style={this.textFieldStyle} InputProps={{ style: { borderColor: '#19d2b3' } }} />
               <TextField id="password" onChange={this.handleChange} type="password" variant="outlined" placeholder="Password" style={this.textFieldStyle} InputProps={{ style: { borderColor: '#19d2b3' } }} />
-              <Chip label="Forgot/reset password" clickable="true" variant="filled" sx={{marginBottom: '10px', width: '174px', color: '#19d2b3'}}></Chip>
+              <Chip label="Forgot/reset password" clickable="true" variant="filled" sx={{marginBottom: '20px', width: '174px', color: '#19d2b3'}}></Chip>
               <Button onClick={this.handleClick} variant="contained" style={this.buttonStyle} sx={{ backgroundColor: '#19d2b3', ':hover': { backgroundColor: 'white', color: '#19d2b3' } }}>Submit</Button>
             </CardContent>
           </Card>
@@ -133,19 +133,19 @@ class Signup extends React.Component {
             <CardContent style={this.containerStyle}>
               <Box sx={{ p: 2, display: 'flex' }}>
               <Stack alignItems="center" >
-              <TextField onChange={this.handleChange} id="name" sx={{ m : '10px', width: '260px' }} type="text" variant="outlined" placeholder="Name" style={this.textFieldStyle} InputProps={{ style: { borderColor: '#19d2b3' } }} />
-              <TextField onChange={this.handleChange} id="prefUsername" sx={{ m : '10px', width: '260px' }} type="text" variant="outlined" placeholder="Preferred username" style={this.textFieldStyle} InputProps={{ style: { borderColor: '#19d2b3' } }} />
+              <TextField onChange={this.handleChange} id="name" sx={{ m : '10px', width: '230px' }} type="text" variant="outlined" placeholder="Name" style={this.textFieldStyle} InputProps={{ style: { borderColor: '#19d2b3' } }} />
+              <TextField onChange={this.handleChange} id="prefUsername" sx={{ m : '10px', width: '230px' }} type="text" variant="outlined" placeholder="Preferred username" style={this.textFieldStyle} InputProps={{ style: { borderColor: '#19d2b3' } }} />
               </Stack>
               </Box>
               <Divider />
               <Box sx={{ p: 2, display: 'flex' }}>
               <Stack alignItems="center" >
-              <TextField onChange={this.handleChange} id="password" sx={{ m : '10px', width: '260px' }} type="text" variant="outlined" placeholder="Password" style={this.textFieldStyle} InputProps={{ style: { borderColor: '#19d2b3' } }} />
-              <TextField onChange={this.handleChange} id="confirmPassword" sx={{ m : '10px', width: '260px' }} type="text" variant="outlined" placeholder="Confirm password" style={this.textFieldStyle} InputProps={{ style: { borderColor: '#19d2b3' } }} />
+              <TextField onChange={this.handleChange} id="password" sx={{ m : '10px', width: '230px' }} type="text" variant="outlined" placeholder="Password" style={this.textFieldStyle} InputProps={{ style: { borderColor: '#19d2b3' } }} />
+              <TextField onChange={this.handleChange} id="confirmPassword" sx={{ m : '10px', width: '230px' }} type="text" variant="outlined" placeholder="Confirm password" style={this.textFieldStyle} InputProps={{ style: { borderColor: '#19d2b3' } }} />
               </Stack>
               </Box>
               <Divider />
-              <TextField onChange={this.handleChange} id="faveColor" sx={{ m : '10px', width: '260px' }} type="text" variant="outlined" placeholder="Your favorite color" style={this.textFieldStyle} InputProps={{ style: { borderColor: '#19d2b3' } }} />
+              <TextField onChange={this.handleChange} id="faveColor" sx={{ m : '10px', width: '230px' }} type="text" variant="outlined" placeholder="Your favorite color" style={this.textFieldStyle} InputProps={{ style: { borderColor: '#19d2b3' } }} />
               <Button variant="contained" style={this.buttonStyle} sx={{ backgroundColor: '#19d2b3', ':hover': { backgroundColor: 'white', color: '#19d2b3' } }}>Submit</Button>
             </CardContent>
           </Card>
